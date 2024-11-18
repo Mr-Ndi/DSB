f8155c5f15b596edeece44370a14e194a632142c
Mbaduko <nsengiyumvaclement247@gmail.com>
2024-11-18 10:26:10 +0200

    feat: Database connection in config.go

---
+++ config/config.go
@@ -0,0 +1,49 @@
+package config
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DatabaseConnection() *mongo.Client {
+
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatalf("Error loading .env file:%v", err)
+	}
+
+	dbURI := os.Getenv("DB_URI")
+
+	if dbURI == "" {
+		log.Fatalf("Connection string is empty")
+	}
+
+	clientOptions := options.Client().ApplyURI(dbURI)
+
+	timeOut, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	client, err := mongo.Connect(timeOut, clientOptions)
+
+	if err != nil {
+		log.Fatalf("Failed to connect database: %v", err)
+	}
+
+	err = client.Ping(timeOut, nil)
+
+	if err != nil {
+		log.Fatalf("Failed to ping connection to db: %v", err)
+	}
+
+	fmt.Println("Successfully connected to database !!")
+
+	return client
+}
--- main.go
+++ main.go
@@ -1,50 +1,9 @@
-// main.go
 package main
 
 import (
-	"context"
-	"dsb/routes"
-	"log"
-	"net/http"
-	"os"
-
-	"github.com/gorilla/mux"
-	"github.com/joho/godotenv"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"dsb/config"
 )
 
-var UserCollection *mongo.Collection
-
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error while loading .env file")
-	}
-
-	clientOptions := options.Client().ApplyURI(os.Getenv("DB_URL"))
-	client, err := mongo.Connect(context.TODO(), clientOptions)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = client.Ping(context.TODO(), nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("We are connected to mongoDB")
-
-	UserCollection = client.Database("digitalsuggestionboxpro").Collection("coffee")
-	r := mux.NewRouter()
-
-	// Pass UserCollection to routes
-	routes.UserRoutes(r, UserCollection)
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080" // Default port if not set
-	}
-
-	log.Printf("Server will start at http://localhost:%s/\n", port)
-	log.Fatal(http.ListenAndServe(":"+port, r))
+	config.DatabaseConnection()
 }
